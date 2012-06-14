6ce121089ca5d231fb5f0e8ea4fbbe7b4ce25c31
Elias Amaral <tolkiendili@gmail.com>
2012-06-14 09:30:47 -0300

    criar/deletar/redimensionar obstáculos, pontos de inicio/fim

--- anim.js
+++ anim.js
@@ -1,6 +1,22 @@
-function $() { return document.getElementById.apply(document, arguments) };
+function $(s) {
+    return document.getElementById(s);
+}
+
+function $t(s) {
+    return document.getElementsByTagName(s)[0];
+}
 
 var R;
+
+var del = false;
+
+function get_random_color() {
+    var h = Math.random();
+    var s = Math.random() * (0.8 - 0.1) + 0.1;
+    var l = Math.random() * (0.8 - 0.2) + 0.2;
+    var k = 'hsl(' + h + ',' + s + ',' + l + ')';
+    return k;
+}
 
 
 Raphael.fn.triangle = function(x, y, size) {
@@ -11,8 +27,34 @@ Raphael.fn.triangle = function(x, y, size) {
 };
 
 function rect(o) {
-    return R.rect(o.x, o.y, o.w, o.h).attr({
-        fill: 'hsb(' + o.c + ', 0.2, 1)'
+    var el = R.rect(o.x, o.y, o.w, o.h).attr({
+        fill: get_random_color(),
+        'stroke-width': 5
+    });
+
+    return el.drag(function (dx, dy) {
+        if (this.orig.resize)
+            this.attr({width: this.orig.w + dx, height: this.orig.h + dy});
+        else
+            this.attr({x: this.orig.x + dx, y: this.orig.y + dy});
+    }, function (x, y) {
+        this.orig = {
+            x: this.attrs.x,
+            y: this.attrs.y,
+            dx: x - this.attrs.x,
+            dy: y - this.attrs.y,
+            w: this.attrs.width,
+            h: this.attrs.height
+        };
+
+        this.orig.resize = check_border(this.orig);
+        this.attr({ opacity: 0.5 });
+    }, function () {
+        this.orig = null;
+        this.attr({ opacity: 1 });
+    }).dblclick(function () {
+        del = true;
+        el.remove();
     });
 }
 
@@ -22,34 +64,59 @@ function trig(o) {
     });
 }
 
+function check_border(o) {
+    var dx = o.w - o.dx;
+    var dy = o.h - o.dy;
+
+    return dx < 5 || dy < 5;
+}
+
+
+var pl, cl, ci;
+
 window.onload = function () {
     R = Raphael(0, 0, "100%", "100%");
 
-    var dashed = {fill: "none", stroke: "#666", "stroke-dasharray": "- "};
+    $t('svg').ondblclick = function(e) {
+        if (del) {
+            del = false;
+            return;
+        }
+        rect({ x: e.x - 50, y: e.y - 50, w: 100, h: 100 });
+    };
 
-    var obstaculos = [
-        rect({ x: 100, y: 100, w: 150, h: 150, c: 0 }),
-        rect({ x: 310, y: 100, w: 100, h: 100, c: .3 }),
-        rect({ x: 420, y: 250, w: 100, h: 150, c: .6 }),
-        rect({ x: 530, y: 100, w: 100, h: 100, c: .9 })
-    ];
+    pl = trig({ x : 0, y : 0, s: 100 }).transform('t100,10');
 
+    ci = R.circle(100, 10, 5).attr({ fill: 'hsl(0.8,1,0.5)' });
 
-      R.set.apply(R, obstaculos).drag(function (dx, dy) {
-        this.attr({x: this.orig.x + dx, y: this.orig.y + dy});
-    }, function () {
-        this.orig = { x: this.attrs.x, y: this.attrs.y };
+    cl = R.circle(300, 100, 5).attr({ fill: 'hsl(0.2,1,0.5)' });
+
+    R.set(ci, cl).drag(function (dx, dy) {
+        this.attr({cx: this.orig.cx + dx, cy: this.orig.cy + dy });
+    },
+    function (x, y) {
+        this.orig = {
+            cx: this.attrs.cx,
+            cy: this.attrs.cy
+        };
         this.attr({ opacity: 0.5 });
     }, function () {
         this.orig = null;
         this.attr({ opacity: 1 });
     });
 
-    var pl = trig({ x : 200, y : 300, s: 100 });
-
-
+    pl.attr({ moved: false });
 
     pl.click(function () {
-        pl.animate({ transform: 't300,-100r-25' }, 2000, '<>');
+        var cit = 't' + ci.attrs.cx + ',' + ci.attrs.cy;
+        var clt = 't' + cl.attrs.cx + ',' + cl.attrs.cy;
+
+        //'t300,-100r-25'
+
+        pl.attrs.moved = ! pl.attrs.moved;
+        if (pl.attrs.moved)
+            pl.stop().animate({ transform: clt }, 2000, '<>');
+        else
+            pl.stop().animate({ transform: cit }, 2000, '<>');
     });
 };
